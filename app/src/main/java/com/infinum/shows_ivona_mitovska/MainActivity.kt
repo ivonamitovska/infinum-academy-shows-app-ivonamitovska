f6a404226b339da916502fddcd348bc2507909fc
Ivona Mitovska <ivonamitovska55@gmail.com>
2022-07-08 20:06:20 +0200

    Pushing Empty Project - Assignment1

---
+++ app/src/main/java/com/infinum/shows_ivona_mitovska/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.infinum.shows_ivona_mitovska
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
