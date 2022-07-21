dbbf48ba117c8204ca6c8698b7ab07a99cf88f22
Ivona Mitovska <ivonamitovska55@gmail.com>
2022-07-21 15:15:27 +0200

    pushing assignment4

---
+++ app/src/main/java/com/infinum/shows_ivona_mitovska/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.infinum.shows_ivona_mitovska.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.infinum.shows_ivona_mitovska.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
